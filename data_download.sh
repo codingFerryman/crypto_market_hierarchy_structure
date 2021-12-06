27186941f26125581b2af0b4c9a446ce22e2c6d9
He Liu <heliu@heliu.name>
2021-12-06 23:33:39 +0100

    Read all the codes; drop empty data in downloader; modify data structure

--- data_download.sh
+++ data_download.sh
@@ -4,9 +4,14 @@
 # Supported date format: YYYYMMDD and YYYY-MM-DD
 # Supported interval: '1m', '5m', '15m', '30m', '1h', '3h', '6h', '12h', '1D', '1W'
 # Please be aware that the end date won't be included
+
+# All codes
+CODES=$(tr -d "\n\r" < cryptocurrency_code.txt)
+
+# Download
 python src/data_downloader.py \
-coin="BTC,ETH,AVAX,SOL,LUNA,LTC,ZEC,TRX,DOT,XRP,CTK" \
-start="20211001" \
-end="20211101" \
-interval="1m" \
+coin=$CODES \
+start="2021-01-13" \
+end="2021-03-14" \
+interval="1W" \
 # output="./output.csv"
