b1c046806874e7a84d0153a4e64e0487fc31691b
Xiaozhe Yao <askxzyao@gmail.com>
2022-02-12 12:31:55 +0000

    update

--- src/backend/web/scripts/import_models.py
+++ src/backend/web/scripts/import_models.py
@@ -16,17 +16,21 @@ for idx, row in image_models.iterrows():
     json_model = str(json.loads(row['json_model']))
     num_params = row['num_params']
     feat_size = row['dimension']
+    up_acc = float(row['up_acc'])
     input_spec = str({
         'image_size': row['image_size'],
     })
+
     res = requests.post(
-            "http://localhost:8081/models",
+            "http://localhost:8080/models",
             json = {
                 "json_model": json_model,
                 "feat_size": feat_size,
                 "input_spec": input_spec,
                 "num_params": num_params,
+                "up_acc": up_acc,
             }
         )
+
     print(res.text)
 
